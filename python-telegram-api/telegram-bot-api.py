64ccf35c50b80709f41b3f8002cc6273342097f2
Eric Damian <damianeric2002@gmail.com>
2021-02-16 12:23:12 +0100

    Created getUpdates method

--- python-telegram-api/telegram-bot-api.py
+++ python-telegram-api/telegram-bot-api.py
@@ -15,3 +15,30 @@ class TelegramBotApi():
     def __init__(self, token: str):
         
         self.token = token # Bot token
+        self.last_update_id = 0
+
+    def _getUpdates(self, offset=0, limit=100, timeout=0) -> Dict:
+        """   
+            Use this method to receive incoming updates using long polling. 
+            
+            :param offset: Identifier of the first update to be returned.
+            :param limit: Limits the number of updates to be retrieved.
+            :param timeout: Timeout in seconds for long polling.
+
+            :type offset: int
+            :type limit: int
+            :type timeout: int
+
+            :return: An Array of Update objects is returned.
+            :rtype: list
+
+            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getUpdates 
+        """
+
+        token = self.token
+
+        updates = requests.get(f"https://api.telegram.org/bot{token}/getUpdates?offset={offset}&limit={limit}&timeout={timeout}")
+        
+        self.last_update_id = updates["result"][-1]["update_id"]
+
+        return updates["result"]
