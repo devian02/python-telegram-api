e2c19f92cca37a5e1f04e9ec6e5c2c5ef482d34e
Eric Damian <damianeric2002@gmail.com>
2021-02-16 23:35:00 +0100

    Added more comments

--- python_telegram_api/telegram_bot_api.py
+++ python_telegram_api/telegram_bot_api.py
@@ -18,10 +18,36 @@ class TelegramBotApi():
         self.lastUpdateId = 0
 
     def getToken(self) -> str:
+        """   
+            Use this method to get your actual bot token. 
+
+            :return: Bot token.
+            :rtype: String
+
+            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getToken 
+        """
+
         return self.token
 
-    def setToken(self, token: str):
-        self.token = token
+    def setToken(self, token: str) -> bool:
+        """   
+            Use this method to set your actual bot token. 
+            
+            :param token: New bot token.
+
+            :type token: String
+
+            :return: True if token has been set correctly.
+            :rtype: Boolean
+
+            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setToken 
+        """
+
+        try:
+            self.token = token
+            return True
+        except:
+            return False
 
     def getUpdates(self, offset=0, limit=100, timeout=0, allowed_updates=[]) -> List:
         """   
@@ -47,12 +73,41 @@ class TelegramBotApi():
 
         updates = requests.get(f"https://api.telegram.org/bot{token}/getUpdates?offset={offset}&limit={limit}&timeout={timeout}&allowed_updates={allowed_updates}").json()
         
-        self.lastUpdateId = updates["result"][-1]["update_id"]
+        if len(updates["result"]) >= 1:
+            # If there are updates available
+
+            self.setLastUpdateId(updates["result"][-1]["update_id"]) # Set lastUpdateId
 
         return updates["result"]
 
     def getLastUpdateId(self) -> int:
+        """   
+            Use this method to get lastUpdateId attribute. 
+
+            :return: lastUpdateId attribute.
+            :rtype: int
+
+            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getLastUpdateId 
+        """
+
         return self.lastUpdateId
 
     def setLastUpdateId(self, lastUpdateId: int):
-        self.lastUpdateId = lastUpdateId
+        """   
+            Use this method to set lastUpdateId attribute. 
+            
+            :param lastUpdateId: New lastUpdateId.
+
+            :type lastUpdateId: int
+
+            :return: True if lastUpdateId has been set correctly.
+            :rtype: Boolean
+
+            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setLastUpdateId
+        """
+
+        try:
+            self.lastUpdateId = lastUpdateId
+            return True
+        except:
+            return False
