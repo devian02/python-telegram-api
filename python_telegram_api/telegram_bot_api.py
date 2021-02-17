b3e965109b500d57e5a952d8cab0918302c2e4ed
Eric Damian <47314529+xSklero@users.noreply.github.com>
2021-02-17 17:11:13 +0100

    Improved some methods and all the comments

--- python_telegram_api/telegram_bot_api.py
+++ python_telegram_api/telegram_bot_api.py
@@ -10,39 +10,43 @@ import requests
 from typing import List, Dict
 
 class TelegramBotApi():
-    """ The implementation of the Telegram APIs Bot """
+    """ The implementation of the Python Telegram APIs Bot """
     
     def __init__(self, token: str):
-        
+        """ Constructor of TelegramBotApi class
+
+        Args:
+            token (str): Bot token from BotFather.
+        """
+
         self.botToken = token # Bot token
         self.lastUpdateId = 0
 
         self.debug = False # Use this variable to enable or disable debugging mode
 
     def getDebugMode(self) -> bool:
-        """   
-            Use this method to get your actual debug mode. 
+        """ Use this method to get your actual debug mode.
+        
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getDebugMode
 
-            :return: Actual debug mode.
-            :rtype: Boolean
-
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getDebugMode
+        Returns:
+            bool: Actual debug mode
         """
 
         return self.debug
 
     def setDebugMode(self, mode: bool) -> bool:
-        """   
-            Use this method to set debug mode. 
-            
-            :param mode: When True all responses from Telegram APIs are printed to console.
+        """ Use this method to set debug mode.
 
-            :type mode: Boolean
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/setDebugMode
 
-            :return: True if mode has been set correctly.
-            :rtype: Boolean
+        Args:
+            mode (bool): If True all responses from Telegram APIs are printed to console.
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setDebugMode 
+        Returns:
+            bool: True if mode has been set correctly.
         """
 
         try:
@@ -52,29 +56,28 @@ class TelegramBotApi():
             return False
 
     def getToken(self) -> str:
-        """   
-            Use this method to get your actual bot token. 
+        """ Use this method to get your actual bot token.
 
-            :return: Bot token.
-            :rtype: String
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getToken 
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getToken 
+        Returns:
+            str: Bot token.
         """
 
         return self.botToken
 
     def setToken(self, token: str) -> bool:
-        """   
-            Use this method to set your actual bot token. 
-            
-            :param token: New bot token.
+        """ Use this method to set your actual bot token.
 
-            :type token: String
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/setToken 
 
-            :return: True if token has been set correctly.
-            :rtype: Boolean
+        Args:
+            token (str): New bot token.
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setToken 
+        Returns:
+            bool: True if token has been set correctly.
         """
 
         try:
@@ -84,28 +87,31 @@ class TelegramBotApi():
             return False
 
     def getUpdates(self, offset=0, limit=100, timeout=0, allowed_updates=[]) -> List:
-        """   
-            Use this method to receive incoming updates using long polling. 
-            
-            :param offset: Identifier of the first update to be returned.
-            :param limit: Limits the number of updates to be retrieved.
-            :param timeout: Timeout in seconds for long polling.
-            :param allowed_updates: List of the update types you want your bot to receive.
+        """ Use this method to receive incoming updates using long polling. 
 
-            :type offset: int
-            :type limit: int
-            :type timeout: int
-            :type allowed_updates: List
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getUpdates
 
-            :return: An Array of Update objects is returned.
-            :rtype: List
+        Args:
+            offset (int, optional): Identifier of the first update to be returned. Defaults to 0.
+            limit (int, optional): Limits the number of updates to be retrieved. Defaults to 100.
+            timeout (int, optional): Timeout in seconds for long polling. Defaults to 0.
+            allowed_updates (list, optional): List of the update types you want your bot to receive. Defaults to [].
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getUpdates 
+        Returns:
+            List: An Array of Update objects is returned.
         """
 
         token = self.botToken
 
-        response = requests.get(f"https://api.telegram.org/bot{token}/getUpdates?offset={offset}&limit={limit}&timeout={timeout}&allowed_updates={allowed_updates}").json()
+        params = (
+            ('offset', offset),
+            ('limit', limit),
+            ('timeout', timeout),
+            ('allowed_updates', allowed_updates)
+        )
+
+        response = requests.get(f"https://api.telegram.org/bot{token}/getUpdates", params=params).json()
         
         if self.debug:
             print(response)
@@ -115,32 +121,34 @@ class TelegramBotApi():
 
             self.setLastUpdateId(response["result"][-1]["update_id"]) # Set lastUpdateId
 
-        return response["result"]
+        if response['ok']:
+            return response['result']
+        else:
+            return {'error': 'Error with getUpdates method. Enable debug mode for more info', 'description': response['description']}
 
     def getLastUpdateId(self) -> int:
-        """   
-            Use this method to get lastUpdateId attribute. 
+        """ Use this method to get lastUpdateId attribute.
 
-            :return: lastUpdateId attribute.
-            :rtype: int
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getLastUpdateId
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getLastUpdateId 
+        Returns:
+            int: lastUpdateId attribute
         """
 
         return self.lastUpdateId
 
-    def setLastUpdateId(self, lastUpdateId: int):
-        """   
-            Use this method to set lastUpdateId attribute. 
-            
-            :param lastUpdateId: New lastUpdateId.
+    def setLastUpdateId(self, lastUpdateId: int) -> bool:
+        """ Use this method to set lastUpdateId attribute.
 
-            :type lastUpdateId: int
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/setLastUpdateId
 
-            :return: True if lastUpdateId has been set correctly.
-            :rtype: Boolean
+        Args:
+            lastUpdateId (int): New lastUpdateId
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setLastUpdateId
+        Returns:
+            [bool]: True if lastUpdateId has been set correctly
         """
 
         try:
@@ -150,65 +158,77 @@ class TelegramBotApi():
             return False
 
     def setWebhook(self, url: str, ip_address='', max_connections=40, allowed_updates=[]) -> bool:
-        """   
-            Use this method to receive incoming updates using long polling. 
-            
-            :param url: HTTPS url to send updates to.
-            :param ip_address: The fixed IP address which will be used to send webhook requests instead of the IP address resolved through DNS.
-            :param max_connections: Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100. Defaults to 40.
-            :param allowed_updates: List of the update types you want your bot to receive.
+        """ Use this method to specify a url and receive incoming updates via an outgoing webhook.
 
-            :type url: String
-            :type ip_address: String
-            :type max_connections: int
-            :type allowed_updates: List
+        Note:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/setWebhook
 
-            :return: Returns True on success.
-            :rtype: Boolean
+        Args:
+            url (str): HTTPS url to send updates to.
+            ip_address (str, optional): The fixed IP address which will be used to send webhook requests instead of the IP address resolved through DNS. Defaults to ''.
+            max_connections (int, optional): Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100. Defaults to 40.
+            allowed_updates (list, optional): List of the update types you want your bot to receive. Defaults to [].
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/setWebhook 
+        Returns:
+            bool: Returns True on success
         """
-
+        
         token = self.botToken
 
-        response = requests.get(f"https://api.telegram.org/bot{token}/setWebhook?url={url}&ip_address={ip_address}&max_connections={max_connections}&allowed_updates={allowed_updates}").json()
+        params = (
+            ('url', url),
+            ('ip_address', ip_address),
+            ('max_connections', max_connections),
+            ('allowed_updates', allowed_updates)
+        )
+
+        response = requests.get(f"https://api.telegram.org/bot{token}/setWebhook", params=params).json()
 
         if self.debug:
             print(response)
 
-        return response['ok']
+        if response['ok']:
+            return response['result']
+        else:
+            return {'error': 'Error with setWebhook method. Enable debug mode for more info', 'description': response['description']}
 
     def deleteWebhook(self, drop_pending_updates=True) -> bool:
-        """   
-            Use this method to remove webhook integration.
-            
-            :param drop_pending_updates: HTTPS url to send updates to.
+        """ Use this method to remove webhook integration.
 
-            :type drop_pending_updates: Boolean
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/deleteWebhook
 
-            :return: Returns True on success.
-            :rtype: Boolean
+        Args:
+            drop_pending_updates (bool, optional): HTTPS url to send updates to. Defaults to True.
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/deleteWebhook
+        Returns:
+            bool: Returns True on success.
         """
 
         token = self.botToken
 
-        response = requests.get(f"https://api.telegram.org/bot{token}/deleteWebhook?drop_pending_updates={drop_pending_updates}").json()
+        params = (
+            ('drop_pending_updates', drop_pending_updates),
+        )
+
+        response = requests.get(f"https://api.telegram.org/bot{token}/deleteWebhook", params=params).json()
 
         if self.debug:
             print(response)
 
-        return response['ok']
+        if response['ok']:
+            return response['result']
+        else:
+            return {'error': 'Error with deleteWebhook method. Enable debug mode for more info', 'description': response['description']}
 
     def getWebhookInfo(self) -> Dict:
-        """   
-            Use this method to get current webhook status.
+        """ Use this method to get current webhook status.
 
-            :return: Returns a WebhookInfo object.
-            :rtype: Dict
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getWebhookInfo
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getWebhookInfo
+        Returns:
+            Dict: Returns a WebhookInfo object.
         """
 
         token = self.botToken
@@ -218,16 +238,19 @@ class TelegramBotApi():
         if self.debug:
             print(response)
 
-        return response['result']
+        if response['ok']:
+            return response['result']
+        else:
+            return {'error': 'Error with getWebhookInfo method. Enable debug mode for more info', 'description': response['description']}
 
     def getMe(self) -> Dict:
-        """   
-            Use this method to get basic bot informations. 
+        """ Use this method to get basic bot informations.
 
-            :return: Returns basic information about the bot in form of a User object.
-            :rtype: Dict
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getMe
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getMe
+        Returns:
+            Dict: Returns basic information about the bot in form of a User object.
         """
 
         token = self.botToken
@@ -237,28 +260,37 @@ class TelegramBotApi():
         if self.debug:
             print(response)
 
-        return response["result"]
+        if response['ok']:
+            return response['result']
+        else:
+            return {'error': 'Error with getMe method. Enable debug mode for more info', 'description': response['description']}
 
     def getBotUsername(self) -> str:
-        """   
-            Use this method to get your actual bot username. 
+        """ Use this method to get your actual bot username. 
 
-            :return: Bot username.
-            :rtype: String
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getBotUsername
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getBotUsername 
+        Returns:
+            str: Bot username.
         """
 
-        return self.getMe()['username']
+        try:
+            return self.getMe()['username']
+        except:
+            return ''
     
     def getBotFirstName(self) -> str:
-        """   
-            Use this method to get your actual bot first name. 
+        """ Use this method to get your actual bot first name.
 
-            :return: Bot first name.
-            :rtype: String
+        Notes:
+            For more info -> https://github.com/xSklero/python-telegram-api/wiki/getBotFirstName
 
-            .. note:: For more info -> https://github.com/xSklero/python-telegram-api/wiki/getBotFirstName
+        Returns:
+            str: Bot first name.
         """
 
-        return self.getMe()['first_name']
+        try:
+            return self.getMe()['first_name']
+        except:
+            return ''
